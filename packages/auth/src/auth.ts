1a3aa63014415ef60d665aba562eaa48d72de425
HitishRaoP <hitishraop@gmail.com>
2025-07-14 20:00:49 +0530

    db setup

--- packages/auth/src/auth.ts
+++ packages/auth/src/auth.ts
@@ -2,10 +2,17 @@ import { backendEnv } from "@webcampus/common/env";
 import { db } from "@webcampus/db";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { username } from "better-auth/plugins";
+import { organization, username } from "better-auth/plugins";
 import { getFileContent } from "./mail/get-file-content";
 import { sendEmail } from "./mail/send-mail";
 
+/**
+ * To fix the typescript error here,
+ * declaration and declarationMap are set to false based on https://github.com/better-auth/better-auth/issues/2123.
+ * This is a workaround until the issue is resolved in the library.
+ * This could be also because there is no option to nohoist a particular package using bun.
+ * https://github.com/oven-sh/bun/issues/6850
+ */
 export const auth = betterAuth({
   database: prismaAdapter(db, {
     provider: "postgresql",
@@ -29,5 +36,5 @@ export const auth = betterAuth({
       });
     },
   },
-  plugins: [username()],
+  plugins: [username(), organization()],
 });
--- packages/db/src/schemas/modelSchema/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./UserSchema";
-export * from "./SessionSchema";
-export * from "./AccountSchema";
-export * from "./VerificationSchema";
