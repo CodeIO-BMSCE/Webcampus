d01f2f003fe767a1f1c783a780f11bfeeeeaca2b
HitishRaoP <hitishraop@gmail.com>
2025-07-24 02:13:35 +0530

    feat: tsup for api build

---
+++ apps/api/tsup.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  dts: true,
+  format: ["esm"],
+  external: [
+    "@webcampus/auth",
+    "@webcampus/types",
+    "@webcampus/common",
+    "@webcampus/schemas",
+    "@webcampus/db",
+    "@webcampus/backend-utils",
+  ],
+});
